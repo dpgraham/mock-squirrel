8ab87d7b0d98c1e5cfdf0e005d5f3551606caabc
dan@saucelabs.com <dan@saucelabs.com>
2018-02-28 21:13:41 -0800

    Add update handler

--- test/unit/api-specs.js
+++ test/unit/api-specs.js
@@ -1,4 +1,4 @@
-import { addElectronRelease, addEntry, latest, PLATFORMS } from '../../lib/api';
+import { addElectronRelease, addEntry, latest, PLATFORMS, reset } from '../../lib/api';
 import chai from 'chai';
 import path from 'path';
 import chaiAsPromised from 'chai-as-promised';
@@ -9,20 +9,25 @@ chai.should();
 chai.use(chaiAsPromised);
 
 describe('api.js', function () {
+  beforeEach(function () {
+    reset();
+  });
   describe('.addEntry', function () {
     it('should reject entries to non-existent platforms', function () {
       (() => addEntry('fakePlatform')).should.throw(/no such platform/);
     });
     it('should add entries by platform', function () {
-      addEntry('darwin', 'v1.1.0', '/fake/path');
+      addEntry('darwin', 'v1.1.0', '/fake/path', 1);
       latest.platforms[PLATFORMS.MAC].should.eql({
         version: 'v1.1.0',
         path: '/fake/path',
+        pubDate: 1,
       });
-      addEntry('darwin', 'v1.2.0', '/a/new/path');
+      addEntry('darwin', 'v1.2.0', '/a/new/path', 1);
       latest.platforms[PLATFORMS.MAC].should.eql({
         version: 'v1.2.0',
         path: '/a/new/path',
+        pubDate: 1,
       });
     });
   });
@@ -30,19 +35,22 @@ describe('api.js', function () {
   describe('.addElectronRelease', function () {
     const release = path.resolve(__dirname, '..', '..', 'test', 'assets', 'release');
 
-    it.only('should save platforms to their respective folders', async function () {
-      await addElectronRelease(release, 'v1.4.0');
-      latest.platforms.darwin.should.eql({
+    it('should save platforms to their respective folders', async function () {
+      await addElectronRelease(release, 'v1.4.0', 1);
+      latest.platforms[PLATFORMS.MAC].should.eql({
         version: 'v1.4.0',
         path: path.resolve(release, 'Appium-1.4.0.dmg'),
+        pubDate: 1,
       });
-      latest.platforms.win32.should.eql({
+      latest.platforms[PLATFORMS.WIN].should.eql({
         version: 'v1.4.0',
         path: path.resolve(release, 'appium-desktop-setup-1.4.0.exe'),
+        pubDate: 1,
       });
-      latest.platforms.linux.should.eql({
+      latest.platforms[PLATFORMS.APPIMAGE].should.eql({
         version: 'v1.4.0',
         path: path.resolve(release, 'appium-desktop-1.4.0-x86_64.AppImage'),
+        pubDate: 1,
       });
     });
   });
---
+++ test/unit/handlers-specs.js
@@ -0,0 +1,56 @@
+import { update } from '../../lib/handlers';
+import { reset, addEntry, PLATFORMS } from '../../lib/api';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('handlers.js', function () {
+  beforeEach(function () {
+    reset();
+  });
+  describe('update', function () {
+    it('should reject invalid platform name with 500 status', async function () {
+      await update({platform: 'bogus'}).should.eventually.eql({status: 500, message: 'Invalid platform'});
+    });
+    it('should find nothing if nothing was created', async function () {
+      await update({platform: 'darwin', version: 'v0.0.1'}).should.eventually.eql({status: 204});
+    });
+    for (let platform of ['mac', 'MAC', 'MacOS', 'Win', 'windows', 'AppImage', 'appImage']) {
+      it(`should find nothing if nothing was created and alias ${platform} was used`, async function () {
+        await update({platform, version: 'v0.0.1'}).should.eventually.eql({status: 204});
+      });
+    }
+    it('should do 204 if latest is same version', async function () {
+      addEntry(PLATFORMS.MAC, '1.1.0', '/fake/path', 1);
+      await update({platform: PLATFORMS.MAC, version: '1.1.0'}).should.eventually.eql({status: 204});
+    });
+    it('should do 204 if latest is same version and should respect SEMVER', async function () {
+      addEntry(PLATFORMS.MAC, '1.1.0', '/fake/path', 1);
+      await update({platform: PLATFORMS.MAC, version: 'v1.1.0'}).should.eventually.eql({status: 204});
+    });
+    it('should do 200 if latest is different from passed-in version', async function () {
+      addEntry(PLATFORMS.MAC, '1.1.0', '/fake/path', 1);
+      await update({platform: PLATFORMS.MAC, version: '1.2.0'}).should.eventually.eql({
+        status: 200,
+        name: '1.1.0',
+        pub_date: 1,
+        // notes: 'afdsafsadd' // TODO: Add release notes
+        // url: `` // TODO: Add URL with BaseURL from a config
+      });
+    });
+    it('should do 200 if latest is different from passed-in version and should respect SEMVER', async function () {
+      addEntry(PLATFORMS.MAC, '1.1.0', '/fake/path', 1);
+      await update({platform: PLATFORMS.MAC, version: 'v1.2.0'}).should.eventually.eql({
+        status: 200,
+        name: '1.1.0',
+        pub_date: 1,
+        // notes: 'afdsafsadd' // TODO: Add release notes
+        // url: `` // TODO: Add URL with BaseURL from a config
+      });
+    });
+
+  });
+
+});
