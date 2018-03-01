abfd12425b3d232aca1b09a429445daf6b03f6fd
dan@saucelabs.com <dan@saucelabs.com>
2018-02-28 20:13:04 -0800

    Can parse contents of Electron release folder and save to 'latest'

--- test/unit/api-specs.js
+++ test/unit/api-specs.js
@@ -1,5 +1,6 @@
-import { addElectronRelease, addEntry, latest } from '../../lib/api';
+import { addElectronRelease, addEntry, latest, PLATFORMS } from '../../lib/api';
 import chai from 'chai';
+import path from 'path';
 import chaiAsPromised from 'chai-as-promised';
 import { fs } from 'appium-support';
 import sinon from 'sinon';
@@ -9,21 +10,39 @@ chai.use(chaiAsPromised);
 
 describe('api.js', function () {
   describe('.addEntry', function () {
-
-    it('should reject entries to non-existent platforms', async function () {
+    it('should reject entries to non-existent platforms', function () {
       (() => addEntry('fakePlatform')).should.throw(/no such platform/);
     });
-
-    it('should add entries by platform', async function () {
+    it('should add entries by platform', function () {
       addEntry('darwin', 'v1.1.0', '/fake/path');
-      latest.platforms.darwin.should.eql({
+      latest.platforms[PLATFORMS.MAC].should.eql({
         version: 'v1.1.0',
         path: '/fake/path',
       });
       addEntry('darwin', 'v1.2.0', '/a/new/path');
-      latest.platforms.darwin.should.eql({
+      latest.platforms[PLATFORMS.MAC].should.eql({
         version: 'v1.2.0',
         path: '/a/new/path',
+      });
+    });
+  });
+
+  describe('.addElectronRelease', function () {
+    const release = path.resolve(__dirname, '..', '..', 'test', 'assets', 'release');
+
+    it.only('should save platforms to their respective folders', async function () {
+      await addElectronRelease(release, 'v1.4.0');
+      latest.platforms.darwin.should.eql({
+        version: 'v1.4.0',
+        path: path.resolve(release, 'Appium-1.4.0.dmg'),
+      });
+      latest.platforms.win32.should.eql({
+        version: 'v1.4.0',
+        path: path.resolve(release, 'appium-desktop-setup-1.4.0.exe'),
+      });
+      latest.platforms.linux.should.eql({
+        version: 'v1.4.0',
+        path: path.resolve(release, 'appium-desktop-1.4.0-x86_64.AppImage'),
       });
     });
   });
